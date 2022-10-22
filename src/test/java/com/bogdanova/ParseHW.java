85bcf7fe5b9de01cad6f5ad69aa1e9813c258987
Natalia-Bogdanova <bogdanowa.vlg@gmail.com>
2022-10-22 20:11:48 +0300

    added

--- src/test/java/com/bogdanova/ParseHW.java
+++ src/test/java/com/bogdanova/ParseHW.java
@@ -1,14 +1,19 @@
 package com.bogdanova;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
 import com.opencsv.CSVReader;
+import jdk.internal.jmod.JmodFile;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,43 +22,44 @@ public class ParseHW {
     ClassLoader cl = ParseHW.class.getClassLoader();
 
     @Test
-    void zipXls() throws Exception {
+    void zipTest() throws Exception {
+        Class<Object> cl;
         InputStream is = cl.getResourceAsStream("TestHW.zip");
         ZipInputStream zis = new ZipInputStream(is);
         ZipEntry entry;
         while ((entry = zis.getNextEntry()) != null) {
-            String name = entry.getName();
-            long size = entry.getSize();
-            System.out.printf("File name: %s \t File size: %d \n", name, size);
-            assertThat(entry.getName()).isEqualTo("Клиники по программе.xlsm");
-            assertThat(entry.getSize()).isEqualTo(68);
-        }
-    }
-
-    @Test
-    void zipCsv() throws Exception {
-        InputStream is = cl.getResourceAsStream("folder/sample4.zip");
-        ZipInputStream zis = new ZipInputStream(is);
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-            String name = entry.getName();
-            long size = entry.getSize();
-            System.out.printf("File name: %s \t File size: %d \n", name, size);
-            assertThat(entry.getName()).isEqualTo("sample4.csv");
-            assertThat(entry.getSize()).isEqualTo(7918);
-        }
-    }
-
-    @Test
-    void zipPdf() throws Exception {
-        InputStream is = cl.getResourceAsStream("folder/PDF_829613669_10.2022_15.19.43.zip");
-        ZipInputStream zis = new ZipInputStream(is);
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-            String namePdf = entry.getName();
-            long size = entry.getSize();
-            assertThat(entry.getName()).isEqualTo("PDF_829613669_10.2022_15.19.43.pdf");
-            System.out.println(entry.getName());
+            Assertions.assertThat(entry.getName()).isEqualTo("Клиники по программе.xlsx");
+            Assertions.assertThat(entry.getName()).isEqualTo("Natalia Bogdanova Communication Profile.pdf");
+            Assertions.assertThat(entry.getName()).isEqualTo("fileCSV.csv");
+            switch (entry.getName()) {
+                case "fileCSV.csv":
+                    JmodFile zf;
+                    try (InputStream inputStream = zf.getInputStream(entry);
+                         CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                        List<String[]> content = reader.readAll();
+                        String[] row = content.get(0);
+                        String searchWords = row[1];
+                        assertThat(searchWords).isEqualTo("Samara");
+                    }
+                    break;
+                case "Клиники по программе.xlsx":
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        XLS xls = new XLS(inputStream);
+                        assertThat(
+                                xls.excel.getSheetAt(1)
+                                        .getRow(1)
+                                        .getCell(2)
+                                        .getStringCellValue()
+                        ).isEqualTo("Samara");
+                    }
+                    break;
+                case "Natalia Bogdanova Communication Profile.pdf":
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        PDF pdf = new PDF(inputStream);
+                        assertThat(pdf.author).isEqualTo("Максим");
+                    }
+                    break;
+            }
         }
     }
 
