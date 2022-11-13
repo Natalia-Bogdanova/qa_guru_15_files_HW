0efd54eef73e4928ce582d448659a1155b202137
Natalia-Bogdanova <bogdanowa.vlg@gmail.com>
2022-11-13 20:26:47 +0300

    added

--- src/test/java/com/bogdanova/ParseHW.java
+++ src/test/java/com/bogdanova/ParseHW.java
@@ -1,67 +1,86 @@
 package com.bogdanova;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParseHW {
+
         ClassLoader cl = ParseHW.class.getClassLoader();
 
+        @DisplayName("Checking PDF-file from ZIP")
         @Test
+        void testPDF() throws Exception {
+                ZipFile zipfile = new ZipFile(new File("src/test/resources/Test_HW.zip"));
+                try (InputStream is = cl.getResourceAsStream("Test_HW.zip");
+                     ZipInputStream zis = new ZipInputStream(is)) {
+                        ZipEntry entry;
+                        while ((entry = zis.getNextEntry()) != null) {
+                                if (entry.getName().contains(".pdf")) {
+                                        try (InputStream inputStream = zipfile.getInputStream(entry)) {
+                                                PDF pdf = new PDF(inputStream);
+                                                assertThat(pdf.author).isEqualTo("Chromium");
+                                                assertThat(pdf.numberOfPages).isEqualTo(8);
+                                        }
+                                }
 
-
-        void zipXls() throws Exception {
-                InputStream is = cl.getResourceAsStream("TestHW.zip");
-                ZipInputStream zis = new ZipInputStream(new ZipInputStream(is));
-                ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-                String name = entry.getName();
-                long size = entry.getSize();
-        System.out.printf("File name: %s \t File size: %d \n", name, size);
-        assertThat(entry.getName()).isEqualTo("TestHW/Клиники по программе.xlsx");
-        assertThat(entry.getSize()).isEqualTo(12288);
+                        }
+                }
         }
-        }
 
+        @DisplayName("Checking XLS-file from ZIP")
         @Test
-        void zipCsv() throws Exception {
-        InputStream is = cl.getResourceAsStream("TestHW.zip");
-        ZipInputStream zis = new ZipInputStream(is);
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-        String name = entry.getName();
-        long size = entry.getSize();
-        System.out.printf("File name: %s \t File size: %d \n", name, size);
-        assertThat(entry.getName()).isEqualTo("fileCSV.csv");
-        assertThat(entry.getSize()).isEqualTo(68);
-        }
+        void zipXlsTest() throws Exception {
+                ZipFile zipfile = new ZipFile(new File("src/test/resources/Test_HW.zip"));
+                try (InputStream is = cl.getResourceAsStream("Test_HW.zip");
+                     ZipInputStream zis = new ZipInputStream(is)) {
+                        ZipEntry entry;
+                        while ((entry = zis.getNextEntry()) != null) {
+                                if (entry.getName().contains(".xlsx")) {
+                                        try (InputStream inputStream = zipfile.getInputStream(entry)) {
+                                                XLS xls = new XLS(inputStream);
+                                                AssertionsForClassTypes.assertThat(
+                                                        xls.excel.getSheetAt(0)
+                                                                .getRow(1)
+                                                                .getCell(2).getStringCellValue()
+                                                ).isEqualTo("Волгоград");
+                                        }
+                                }
+                        }
+                }
         }
 
+        @DisplayName("Checking CSV-file from ZIP")
         @Test
-        void zipPdf() throws Exception {
-        InputStream is = cl.getResourceAsStream("TestHW.zip");
-        ZipInputStream zis = new ZipInputStream(new ZipInputStream(is));
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-        String namePdf = entry.getName();
-        long size = entry.getSize();
-        assertThat(entry.getName()).isIn("Natalia Bogdanova Communication Profile.pdf");
-        System.out.println(entry.getName());
+        void zipCsvTest() throws Exception {
+                ZipFile zipfile = new ZipFile(new File("src/test/resources/Test_HW.zip"));
+                try (InputStream is = cl.getResourceAsStream("Test_HW.zip");
+                     ZipInputStream zis = new ZipInputStream(is)) {
+                        ZipEntry entry;
+                        while ((entry = zis.getNextEntry()) != null) {
+                                if (entry.getName().contains(".csv")) {
+                                        try (InputStream inputStream = zipfile.getInputStream(entry);
+                                             CSVReader reader = new CSVReader(new InputStreamReader(inputStream))) {
+                                                List<String[]> content = reader.readAll();
+                                                String[] row = content.get(2);
+                                                assertThat(row[0]).isEqualTo("Nothing is clear");
+                                                assertThat(row[1]).isEqualTo("Very interesting");
+                                        }
+                                }
+                        }
+                }
         }
-        }
-
-
         }
